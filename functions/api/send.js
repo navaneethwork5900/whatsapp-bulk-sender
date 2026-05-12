30bbc5879969e7879cdc5c90457a3ed73225b6d3
navaneethwork5900 <navaneethwork5900@gmail.com>
2026-05-12 16:50:14 +0530

    Update send.js

--- functions/api/send.js
+++ functions/api/send.js
@@ -34,6 +34,29 @@ export async function onRequestPost(context) {
         if (updatedCount >= 5) {
 
             templateName = "hello_world";
+            const response = await fetch(
+            `https://graph.facebook.com/v25.0/${context.env.PHONE_NUMBER_ID}/messages`,
+            {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${context.env.WHATSAPP_TOKEN}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    messaging_product: 'whatsapp',
+                    to: mobile,
+                    type: 'template',
+                    template: {
+                        name: templateName,
+                        language: {
+                            code: 'en_US'
+                        }
+                    }
+                })
+            }
+        );
+
+        const data = await response.json();
         }
 
         // Send WhatsApp message
